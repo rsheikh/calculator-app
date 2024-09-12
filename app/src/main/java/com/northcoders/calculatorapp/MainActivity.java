c6c90370fb56e2708d24eec46d05884f372bc8a1
rsheikh <red.sheikh@gmail.com>
2024-09-12 16:52:15 +0100

    feat: update strings resource file and implemented logic for adding two inputted numbers

--- app/src/main/java/com/northcoders/calculatorapp/MainActivity.java
+++ app/src/main/java/com/northcoders/calculatorapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.northcoders.calculatorapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button button = findViewById(R.id.buttonCalculate);
+        EditText textInput = findViewById(R.id.TextInput);
+        EditText textInput2 = findViewById(R.id.TextInput2);
+        TextView resultMessage = findViewById(R.id.addedResult);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              result = addValues(textInput, textInput2);
+              Log.i("****PRINT RESULT", ""+result);
+
+              resultMessage.setText(String.format("The answer is %s ", result));
+            }
+        });
+
+    }
+
+    private int addValues(EditText textInput, EditText textInput2) {
+
+        int firstNum = Integer.parseInt(textInput.getText().toString());
+        int secondNum = Integer.parseInt(textInput2.getText().toString());
+
+        return firstNum + secondNum;
     }
 }
